e33d1fb26fd5fd761502c1080c4a162a86b16e45
Souvik Bhattacharyya <57744600+Souvik-Bhattacharyya@users.noreply.github.com>
2024-04-19 18:25:44 +0530

    mobile responsive

--- script.js
+++ script.js
@@ -1,1 +1,71 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    centeredSlides: false,
+    slidesPerGroupSkip: 1,
+    grabCursor: true,
+    loop: true,
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        300: {
+            slidesPerView: 1,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+        },
+    },
+});
 
+const hamburger = document.querySelector(".hamburger");
+const navUl = document.querySelector(".nav-ul");
+const list = document.querySelectorAll(".nav-list");
+
+hamburger.addEventListener('click', () => {
+    //Animate Links
+    navUl.classList.toggle("open");
+    list.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
+
+let sec = document.querySelectorAll('section');
+let links = document.querySelectorAll('nav a');
+
+window.onscroll = () => {
+    sec.forEach(section => {
+        let top = window.scrollY;
+        let offset = section.offsetTop - 60;
+        let height = section.offsetHeight;
+        let id = section.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            links.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('nav a[href*=' + id + ']').classList.add('active');
+            })
+        }
+    })
+};
+
+
+const triggerTabList = document.querySelectorAll('#myTab a')
+triggerTabList.forEach(triggerEl => {
+    const tabTrigger = new bootstrap.Tab(triggerEl)
+
+    triggerEl.addEventListener('click', event => {
+        event.preventDefault()
+        tabTrigger.show()
+    })
+});
